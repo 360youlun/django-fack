73a94339154ff208c13ed190e2d1139a2ee49ec5
alexeykuz <a.kuznetsov.light@gmail.com>
2015-07-19 19:32:17 +0300

    Support for custom auth model

--- fack/models.py
+++ fack/models.py
@@ -1,7 +1,11 @@
 import datetime
 from django.db import models
 from django.utils.translation import ugettext_lazy as _
-from django.contrib.auth.models import User
+from django.conf import settings
+try:
+    User = settings.AUTH_USER_MODEL
+except ImportError:
+    from django.contrib.auth.models import User
 from django.template.defaultfilters import slugify
 from managers import QuestionManager
 
